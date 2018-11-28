cfb5dad570716c70ae38119e9b5b7e3ee9291224
andreasgtech <andreasgtech@gmail.com>
2018-11-28 20:51:43 +0200

    Metadata structures added

---
+++ metadataColumn.h
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include "radixHashJoin.h"
+
+typedef struct metadataCol {
+  uint32_t num_of_rows;
+  int32_t min, max;
+  uint32_t discrete_values;
+
+  tuple * column; //this points to an array of tuples
+} metadataCol;
+
+metadataCol * initializeMDCol(relation * rel);
+
+int getDiscreteValues(metadataCol * md);
+
+int getMinValue(metadataCol * md);
+
+int getMaxValue(metadataCol * md);
+
+int getNumOfRows(metadataCol * md);
+
+void deleteMDCol(metadataCol * md);
