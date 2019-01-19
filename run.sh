d4c8c68d4109bdf0fa558de3e8ac40a416d31157
PiStefania <stefania.patsou@hotmail.com>
2019-01-20 01:52:37 +0200

    Changed run script and created statistics script

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+args=("$@")
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-DATA_DIR="small"
-${DIR}/build/release/joinProgram -i ./workloads/"$DATA_DIR"/small.init -w ./workloads/"$DATA_DIR"/small.work
+DATA_DIR="${args[0]}"
+statistic="${args[1]}"
+statisticTool="${args[2]}"
+logFile="${args[3]}"
+
+$statistic $statisticTool $logFile ${DIR}/build/release/joinProgram -i ./workloads/"$DATA_DIR"/small.init -w ./workloads/"$DATA_DIR"/small.work
