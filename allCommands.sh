714836dfabfb4838ad8f878bad27c2e80066011e
Pantokratoras7 <orestispgarmpis@yahoo.com>
2019-01-20 20:33:23 +0200

    Remove useless structure

--- allCommands.sh
+++ allCommands.sh
@@ -4,49 +4,6 @@
 echo "---------COMPILE PROGRAM---------"
 ./compile.sh
 
-echo "---------RUN TEST SMALL FOLDER"
-# run run.sh with time
-echo "---------with time-------------"
-./run.sh testSmall time	&>> statistics/testSmallLogs.txt
-
-# run run.sh with valgrind --leak-check=full
-echo "---------with leak check-------------"
-./run.sh testSmall valgrind --leak-check=full &>> statistics/testSmallLogs.txt
-
-# run run.sh with valgrind --tool=callgrind
-echo "---------with callgrind-------------"
-./run.sh testSmall valgrind --tool=callgrind --callgrind-out-file=statistics/callgrindTestSmall &>> statistics/testSmallLogs.txt
-
-# run run.sh with valgrind --tool=massif
-echo "---------with massif-------------"
-./run.sh testSmall valgrind --tool=massif --massif-out-file=statistics/massifTestSmall &>> statistics/testSmallLogs.txt
-
-echo "---------RUN VERY SMALL FOLDER"
-# run run.sh with time
-echo "---------with time-------------"
-./run.sh verySmall time &>> statistics/verySmallLogs.txt
-
-# run run.sh with valgrind --leak-check=full
-echo "---------with leak check-------------"
-./run.sh verySmall valgrind --leak-check=full &>> statistics/verySmallLogs.txt
-
-# run run.sh with valgrind --tool=callgrind
-echo "---------with callgrind-------------"
-./run.sh verySmall valgrind --tool=callgrind --callgrind-out-file=statistics/callgrindVerySmall &>> statistics/verySmallLogs.txt
-
-# run run.sh with valgrind --tool=massif
-echo "---------with massif-------------"
-./run.sh verySmall valgrind --tool=massif --massif-out-file=statistics/massifVerySmall &>> statistics/verySmallLogs.txt
-
-echo "---------RUN TEST SMALL FOLDER"
-# run run.sh with time
-echo "---------with time-------------"
-./run.sh small time &>> statistics/smallLogs.txt
-
-# run run.sh with valgrind --leak-check=full
-echo "---------with leak check-------------"
-./run.sh small valgrind --leak-check=full &>> statistics/smallLogs.txt
-
 # run run.sh with valgrind --tool=callgrind
 echo "---------with callgrind-------------"
 ./run.sh small valgrind --tool=callgrind --callgrind-out-file=statistics/callgrindSmall &>> statistics/smallLogs.txt
@@ -57,6 +14,4 @@ echo "---------with massif-------------"
 
 # run harness
 echo "---------RUN HARNESS FOR ALL---------"
-./runTestharness.sh workloads/testSmall &>> statistics/HarnessLogs.txt
-./runTestharness.sh workloads/verySmall &>> statistics/HarnessLogs.txt
 ./runTestharness.sh workloads/small &>> statistics/HarnessLogs.txt
--- include/statisticsMethods.h
+++ include/statisticsMethods.h
@@ -5,15 +5,6 @@
 #include "queryMethods.h"
 
 
-typedef struct predicatesTree {
-    int numOfTotalPredicates;
-    int numOfCurrentCombinedPredicates;
-    struct predicatesTree* parent;
-    struct predicatesTree** children;      // remaining predicates as children
-    int* predicatesInNode;          // 1 in predicate's index, if current node combination consists this one, 0 otherwise
-    int cost;
-} predicatesTree;
-
 
 int checkCompareStatistics(predicate** predicates, metadataCol** queryMetadata, metadataCol* oldMetadata, int currentPredicate, int relationId, int relColumn);
 uint32_t updateCompareStatistics(predicate** predicates, relationsInfo* initRelations, metadataCol** queryMetadata, metadataCol* oldMetadata,
